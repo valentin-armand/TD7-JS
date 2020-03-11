2e78a477a8318192cfa06d4301160981c2da87e5
Valentin <valentinarm0742@gmail.com>
2020-03-11 14:06:39 +0100

    Début td7

--- src/php/Conf.php
+++ src/php/Conf.php
@@ -3,10 +3,10 @@
 class Conf {
 
     private static $database = array(
-        'hostname' => 'webinfo.iutmontp.univ-montp2.fr',
-        'database' => '...', // à compléter avec vos données personnelles
-        'login'    => '...', // à compléter avec vos données personnelles
-        'password' => '...'  // à compléter avec vos données personnelles
+        'hostname' => 'localhost',
+        'database' => 'td7',
+        'login'    => 'root', // à compléter avec vos données personnelles
+        'password' => ''  // à compléter avec vos données personnelles
     );
 
     static public function getLogin() {
